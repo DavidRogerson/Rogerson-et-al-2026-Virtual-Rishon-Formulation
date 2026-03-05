28b2ea0e077bbcc1ba270f18e14ae7b547ea6fcb
David M. Rogerson <dmr369@fermion80.physics.rutgers.edu>
2026-03-04 19:51:43 -0500

    Increased version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Rogerson_et_al_2026_Virtual_Rishon_Formulation",
-    version="0.1.0",
+    version="1.0.0",
     description="Source code for data generation and analysis of the publication 'Simulating Lattice Gauge Theories with Virtual Rishons' Rogerson et. al. 2026",
     author="David Rogerson",
     author_email="david.rogerson@rutgers.edu",
